b8329b52b3c7961b22bb212fcb82b669140401b6
Md. Shofiul Islam <shofiul.islam@northsouth.edu>
2021-08-14 01:09:05 +0600

    #5: view binding added in SigninupActivity.kt file

--- app/src/main/java/com/shofiul/proman/SigninupActivity.kt
+++ app/src/main/java/com/shofiul/proman/SigninupActivity.kt
@@ -2,10 +2,13 @@ package com.shofiul.proman
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.shofiul.proman.databinding.ActivitySigninupBinding
 
 class SigninupActivity : AppCompatActivity() {
+    lateinit var binding:ActivitySigninupBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signinup)
+        binding = ActivitySigninupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
