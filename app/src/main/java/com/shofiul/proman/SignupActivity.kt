2caa49ce0cf8fe6bbc25afd9d60a1ed7fd38d38f
Md. Shofiul Islam <shofiul.islam@northsouth.edu>
2021-08-14 01:06:06 +0600

    #4: toolbar implemented in the SignupActivity.kt file

--- app/src/main/java/com/shofiul/proman/SignupActivity.kt
+++ app/src/main/java/com/shofiul/proman/SignupActivity.kt
@@ -2,10 +2,16 @@ package com.shofiul.proman
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.shofiul.proman.databinding.ActivitySignupBinding
 
 class SignupActivity : AppCompatActivity() {
+    lateinit var binding: ActivitySignupBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.signupToolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setHomeAsUpIndicator(R.drawable.toolbar_icon_24)
     }
 }
