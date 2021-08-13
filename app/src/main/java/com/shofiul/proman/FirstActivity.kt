f4d7617454f751950d9b5e535228eefa55a36316
Md. Shofiul Islam <shofiul.islam@northsouth.edu>
2021-08-13 19:13:30 +0600

    #3: signin activity implemented

--- app/src/main/java/com/shofiul/proman/FirstActivity.kt
+++ app/src/main/java/com/shofiul/proman/FirstActivity.kt
@@ -1,8 +1,11 @@
 package com.shofiul.proman
 
+import android.content.Intent
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.shofiul.proman.databinding.ActivityFirstBinding
 
 class FirstActivity : AppCompatActivity() {
@@ -13,5 +16,14 @@ class FirstActivity : AppCompatActivity() {
         setContentView(binding.root)
         val font:Typeface = Typeface.createFromAsset(assets,"fonts/Roboto-Bold.ttf")
         binding.tvProman.typeface = font
+
+
+        // Handler do something about the specific time, like setTimeOut in js
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this,SigninupActivity::class.java))
+            finish()
+        }, 1500)
+
+
     }
 }
