1d0424a0a1a049117bbbecdc87330d9bdbc3f827
Md. Shofiul Islam <shofiul.islam@northsouth.edu>
2021-08-13 16:17:33 +0600

    #2: viewBinding added in the FirstActivity.kt file

--- app/src/main/java/com/shofiul/proman/FirstActivity.kt
+++ app/src/main/java/com/shofiul/proman/FirstActivity.kt
@@ -2,10 +2,13 @@ package com.shofiul.proman
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.shofiul.proman.databinding.ActivityFirstBinding
 
 class FirstActivity : AppCompatActivity() {
+    lateinit var binding:ActivityFirstBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_first)
+        binding = ActivityFirstBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
